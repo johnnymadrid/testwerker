052d6344afb68ba3252fd221f40df3e738e334d1
Johnny Madrid <madrid.johnny@gmail.com>
2018-02-18 19:13:37 +0000

    Added Test Port-Functionality

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 var http = require('http');
 
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
+
+app.get('/', function(req, res){
+  res.send('Testing Node Application');
+});
+
+var server = app.listen(3000, function(){
+  console.log('Magic is happening on port 3000');
+});
+
+exports.closeServer = function(){
+  server.close();
+};
+
 function onRequest(request, response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.write('Hello World. Testing Werker');
